2b63edaef9eefccec063b4dcfc7d753c518e4ec7
Max <MaxGroenenboom@users.noreply.github.com>
2016-03-29 01:56:28 +0200

    Added the meta.js file necessary for updating.

---
+++ PWGuard.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        PWGuard
+// @namespace   MG
+// @description Warns the user for non-secure password fields.
+// @include     http:*
+// @version     1.0.4
+// @grant       none
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @updateURL   https://github.com/MaxGroenenboom/pw-guard/raw/master/PWGuard.meta.js
+// @downloadURL https://github.com/MaxGroenenboom/pw-guard/raw/master/PWGuard.user.js
+// ==/UserScript==
